91eac866541462f9f16fd5dfbd9b8b6046e24281
TomTurbo2016 <gemology@live.at>
2019-05-14 17:47:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ import imageResize2
 
 ####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~To-Be-Edited~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
 START_URL = 'https://gexvoquart.onrender.com'
-#PATH_TO_BASE64_TXT_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static/base64strings/')
-PATH_TO_BASE64_TXT_FOLDER = '/opt/render/project/src/static/base64strings/'
+PATH_TO_BASE64_TXT_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static/base64strings/')
 PATH_TO_STYLE_FILES = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static/styleModels/')
 PATH_TO_SCALE_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static/upscaleModel/')
 
