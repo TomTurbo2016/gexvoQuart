9c07d2146213588352668d625584a2768e0952c5
TomTurbo2016 <gemology@live.at>
2019-05-24 13:19:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -197,15 +197,15 @@ async def ShowPic():
                 selectedStyle = (await request.form)['stylize']
                 if selectedStyle == 'mosaic':
                     downloadFileMosaic()
-                    img = await stylize2.main(ioFile, 'mosaic', PATH_TO_STYLE_FILES)
+                    img = stylize2.main(ioFile, 'mosaic', PATH_TO_STYLE_FILES)
                 elif selectedStyle == 'churchWindow':
                     downloadFileChurchwindow()
-                    img = await stylize2.main(ioFile, 'churchWindow', PATH_TO_STYLE_FILES)
+                    img = stylize2.main(ioFile, 'churchWindow', PATH_TO_STYLE_FILES)
                 else:
                     return await redirect(url_for('style_error_nostyle'))
 ####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<
                 downloadFile2xSize()
-                img = await upscale2.main(img, PATH_TO_SCALE_FILE + '2xSize.pth')
+                img = upscale2.main(img, PATH_TO_SCALE_FILE + '2xSize.pth')
                 img = imageResize2.main2(img) #--> 1/3 downscale
                 img = Image.fromarray(img)#.astype("uint8")
                 rawBytes = BytesIO()
